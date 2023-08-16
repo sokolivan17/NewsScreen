e88c047519875fa820034ef938081b659a21199b
Ivan Sokol <sokolivan34@mail.ru>
2023-08-16 12:22:48 +0300

    Deleted main file, clean code.

--- NewsScreen/ViewController.swift
+++ NewsScreen/ViewController.swift
@@ -11,9 +11,10 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        view.backgroundColor = .systemBackground
+        title = "News"
+        navigationController?.navigationBar.prefersLargeTitles = true
     }
-
-
 }
 
