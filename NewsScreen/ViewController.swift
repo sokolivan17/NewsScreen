e5e42b358705312696261821a9ad47dadc02de4b
Ivan Sokol <sokolivan34@mail.ru>
2023-08-16 12:12:50 +0300

    Initial Commit

---
+++ NewsScreen/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NewsScreen
+//
+//  Created by Ваня Сокол on 16.08.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
