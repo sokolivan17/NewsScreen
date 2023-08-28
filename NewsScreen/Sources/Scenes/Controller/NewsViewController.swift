71cc257652faf5d80c8a401507ef273ba51c70dd
Ivan Sokol <sokolivan34@mail.ru>
2023-08-28 23:49:02 +0300

    Make some fix.

--- NewsScreen/Sources/Scenes/Controller/NewsViewController.swift
+++ NewsScreen/Sources/Scenes/Controller/NewsViewController.swift
@@ -67,7 +67,7 @@ extension NewsViewController: UITableViewDataSource, UITableViewDelegate {
         return cell
     }
 
-    func tableView(_ tableView: UITableView, didDeselectRowAt indexPath: IndexPath) {
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         tableView.deselectRow(at: indexPath, animated: true)
         let article = articles[indexPath.row]
 
